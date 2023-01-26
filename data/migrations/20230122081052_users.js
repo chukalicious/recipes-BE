c7a4e1be8ad1be93127139dd0a7c2a5b05fe2639
Katiuska Alicea <katiuskaalicea@gmail.com>
2023-01-25 18:57:02 -0800

    I accedentally started working on the wrong branch. The work done was in the database. I created a table for the recipes and for the ingredients, with one simple endpoint to see if it is connected properly

--- data/migrations/20230122081052_users.js
+++ data/migrations/20230122081052_users.js
@@ -1,13 +1,43 @@
 exports.up = function (knex) {
-  return knex.schema.createTable("users", (tbl) => {
-    tbl.increments("id");
-    tbl.string("email").notNullable().unique();
-    tbl.string("username").unique();
-    tbl.string("password", 256).notNullable();
-    tbl.text("avatar");
-  });
+  return knex.schema
+    .createTable("users", (tbl) => {
+      tbl.increments("id");
+      tbl.string("email").notNullable().unique();
+      tbl.string("username").unique();
+      tbl.string("password", 256).notNullable();
+      tbl.text("avatar");
+    })
+    .createTable("recipes", (tbl) => {
+      tbl.increments("id");
+      tbl.string("recipe_name");
+      tbl
+        .integer("created_by")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("users")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+    })
+    .createTable("ingredients", (tbl) => {
+      tbl.increments("id");
+      tbl.string("ingredient", 256).notNullable();
+      tbl.float("quantity", 4, 2);
+      tbl.string("unit");
+      tbl
+        .integer("recipe_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("recipes")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+    });
 };
 
 exports.down = function (knex) {
-  return knex.schema.dropTableIfExists("users");
+  return knex.schema
+    .dropTableIfExists("ingredients")
+    .dropTableIfExists("recipes")
+    .dropTableIfExists("users");
 };
