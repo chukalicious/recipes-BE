d7865a3ec03bd3f85ed20cdc016cc9356df28d3f
Katiuska Alicea <katiuskaalicea@gmail.com>
2023-01-30 18:19:05 -0800

    added user_id column to the recipe table, created 3 methods for the recipes api, created router for getting recipes for ID and post to recipes

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -2,8 +2,17 @@ const knex = require("knex");
 const config = require("../../knexfile.js");
 const db = knex(config.development);
 
-module.exports = { getAll };
+module.exports = { findAll, findByID, insert };
 
-function getAll() {
+function findAll() {
   return db("recipes");
+}
+
+function findByID(id) {
+  return db("recipes").where({ id }).first();
+}
+
+async function insert(recipe) {
+  const [newRecipe] = await db("recipes").insert(recipe, "*");
+  return findByID(newRecipe.id);
 }
--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -3,7 +3,7 @@ const Recipes = require("./recipes-model");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  Recipes.getAll()
+  Recipes.findAll()
     .then((recipes) => {
       res.status(200).json(recipes);
     })
@@ -11,6 +11,36 @@ router.get("/", (req, res) => {
       console.log(err);
       res.status(401).json({ message: `Server Error` });
     });
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  Recipes.findByID(id)
+    .then((rec) => {
+      if (!rec) {
+        res.status(401).json({ message: `you must enter a recipe` });
+      } else {
+        res.status(201).json(rec);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: `Server Error` });
+    });
+});
+
+router.post("/", (req, res) => {
+  let recipe = req.body;
+  try {
+    if (!recipe) {
+      res.status(401).json({ message: `You must enter a recipe` });
+    } else {
+      res.status(201).json(recipe);
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: `Server Error` });
+  }
 });
 
 module.exports = router;
