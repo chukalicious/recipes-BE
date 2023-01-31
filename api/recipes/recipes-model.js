85bbd9eca1c90d34e32ac22533480c9afd94b96b
Katiuska Alicea <katiuskaalicea@gmail.com>
2023-01-31 00:03:49 -0800

    change api route to the ingredients post method, altered the findByUserID in the recipes model

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -2,24 +2,28 @@ const knex = require("knex");
 const config = require("../../knexfile.js");
 const db = knex(config.development);
 
-module.exports = { findAll, findByID, insert, update, remove };
+module.exports = { findAll, findByID, findByUserID, insert, update, remove };
 
 function findAll() {
-  return db("recipes");
+  return db("recipes").orderBy("id");
 }
 
 function findByID(id) {
   return db("recipes").where({ id }).first();
 }
 
+function findByUserID(created_by) {
+  return db("recipes").where({ created_by });
+}
+
 async function insert(recipe) {
   const [newRecipe] = await db("recipes").insert(recipe, "*");
-  return findByID(newRecipe.id);
+  return findByUserID(newRecipe.created_by);
 }
 
 async function update(id, change) {
   const [updated] = await db("recipes").where({ id }).update(change, "*");
-  return findByID(updated.id);
+  return findByUserID(updated.created_by);
 }
 
 async function remove(id) {
