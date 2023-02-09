b7fa515782ca22bdd1eb45a3bb669477411d560e
Katiuska Alicea <katiuskaalicea@gmail.com>
2023-02-09 03:31:57 -0800

    user can login using the API and sessions are enabled in the server.js

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -4,63 +4,67 @@ const jwt = require("json-web-token");
 const Users = require("../users/users-model");
 const router = express.Router();
 
-const saltRounds = 10;
-const salt = bcrypt.genSaltSync(saltRounds);
+// const saltRounds = 12;
+// const salt = bcrypt.genSaltSync(saltRounds);
 
 // Register
+// router.post("/", async (req, res) => {
+//   let user = req.body;
+
+//   const hash = bcrypt.hashSync(user, salt);
+//   console.log("hash: ", hash);
+
+//   user.password = hash;
+//   console.log("hash: ", hash);
+
+//   try {
+//     const registered = await Users.add(user);
+//     console.log("registered: ", registered);
+//     if (!registered.email || !registered.password) {
+//       res.status(400).json({ message: "Request must include required fields" });
+//     } else {
+//       res.status(201).json(registered);
+//     }
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).json({
+//       message: "Could not create user",
+//       detail: err.detail,
+//       table: `In ${err.table} table`,
+//     });
+//   }
+// });
+
+// Login /////////
 router.post("/", async (req, res) => {
-  let user = req.body;
-
-  const hash = bcrypt.hashSync(user.password, salt);
-
-  user.password = hash;
-  console.log("hash: ", hash);
+  const { email, password } = req.body;
 
   try {
-    const registered = await Users.add(user);
-    console.log("registered: ", registered);
-    if (!registered.email || !registered.password) {
-      res.status(400).json({ message: "Request must include required fields" });
+    // This line below was my problem. I needed to add the .first()
+    const user = await Users.findBy({ email }).first();
+
+    if (user == null) {
+      res.status(401).json({ status: 401, message: "Invalid Credentials" });
+      return;
+    }
+    console.log("password: ", typeof password.toString());
+    if (bcrypt.compareSync(password.toString(), user.password)) {
+      req.session.user = user;
+      res.json({ message: `You are now logged in, ${email}` });
     } else {
-      res.status(201).json(registered);
+      res.status(401).json({ message: "Invalid Credentials" });
     }
   } catch (err) {
     console.log(err);
-    res.status(500).json({
-      message: "Could not create user",
-      detail: err.detail,
-      table: `In ${err.table} table`,
-    });
+    res.status(500).json({ message: "Server Error", err });
   }
 });
 
-// Login
-router.post("/", async (req, res) => {
-  const user = req.body;
-  const { email, password } = req.body;
-  console.log("email and password in the login: ", email, password);
+// Logout
 
-  //   const hash = bcrypt.hashSync(password, salt);
-
-  //   user.password = hash;
-  //   console.log("hash: ", hash);
-  try {
-    const user = await Users.findBy({ email }).first();
-    console.log("user: ", user);
-    if (user == null) {
-      res.status(401).json({ message: `Invalid credentials` });
-      return;
-    }
-    if (bcrypt.compareSync(password, user.password)) {
-      req.session.user = user;
-      res.json({ message: `You are now logged in, ${email}!` });
-    } else {
-      res.status(401).json({ message: `Invalid credentials` });
-    }
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ message: `Server Error` });
-  }
+router.get("/logout", (req, res) => {
+  req.session.destroy();
+  res.json({ message: "you are now logged out" });
 });
 
 // The below fn is for use with JWT
--- api/server.js
+++ api/server.js
@@ -11,22 +11,22 @@ const IngredientsRouter = require("./ingredients/ingredients-router");
 const RecipesRouter = require("./recipes/recipes-router");
 const StepsRouter = require("./steps/steps-router");
 
-// const sessionConfiguration = {
-//   name: "sessionToken",
-//   secret: process.env.SECRET,
-//   cookie: {
-//     maxAge: 1000 * 60 * 120,
-//     secure: false,
-//     httpOnly: true,
-//   },
-//   resave: false,
-//   saveUnititialized: false,
-// };
+const sessionConfiguration = {
+  name: "sessionToken",
+  secret: process.env.SECRET,
+  cookie: {
+    maxAge: 1000 * 60 * 120,
+    secure: false,
+    httpOnly: true,
+  },
+  resave: false,
+  saveUnititialized: false,
+};
 
 server.use(express.json());
 server.use(helmet());
 server.use(cors());
-// server.use(session(sessionConfiguration));
+server.use(session(sessionConfiguration));
 
 server.use("/users", UsersRouter);
 server.use("/signup", AuthRouter);
